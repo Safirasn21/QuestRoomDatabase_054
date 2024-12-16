1f948b91f44fe85717fae4d330419af6c249ca7a
safirasn21 <safirasn021@gmai.com>
2024-12-17 02:00:19 +0700

    melengkapi daftar viewmodel yang sudah dibuat pada folder viewmodel

--- app/src/main/java/com/example/myapplication/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,7 +15,25 @@ object PenyediaViewModel{
                 KrsApp().containerApp.repositoryMhs
             )
         }
+        initializer {
+            MahasiswaHomeViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
     }
 }
 fun CreationExtras.krsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
+
