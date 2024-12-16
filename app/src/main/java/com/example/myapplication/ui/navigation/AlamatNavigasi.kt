53a6206235624be087702bc45727fcdb6eac77b8
safirasn21 <safirasn021@gmai.com>
2024-12-16 23:40:51 +0700

    menambahkan object untuk memindai view

--- app/src/main/java/com/example/myapplication/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/myapplication/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,19 @@ package com.example.myapplication.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArgs = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArgs = "$route/{$NIM}"
+}
